86b1aef4ec89c5546dfbbe9458175b9037b7abe4
Evan Biederstedt <evan.biederstedt@gmail.com>
2018-01-11 16:32:09 -0500

    removed install_bioc.sh

--- install_bioc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-Rscript -e 'source("https://bioconductor.org/biocLite.R"); biocLite("BiocInstaller"); biocLite("BSgenome.Hsapiens.UCSC.hg19"); biocLite("Biostrings"); install.packages("devtools"); biocLite("GenomicRanges"); devtools::install_github("mskilab/gUtils"); devtools::install_github("jimhester/covr")'

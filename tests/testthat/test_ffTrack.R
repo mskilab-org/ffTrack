94ae09ccae599c8054e3b25c092abdf36fcfc48c
evanbiederstedt <evanbiederstedt@users.noreply.github.com>
2018-01-14 13:44:30 -0500

    fixing tab/space issue in test_ffTrack.R

--- tests/testthat/test_ffTrack.R
+++ tests/testthat/test_ffTrack.R
@@ -6,22 +6,22 @@ context('ffTrack operations')
 
 
 test_that('ffTrack', {
-	expect_error(ffTrack(vmode = 'error')) ## Error: Incorrect argument "vmode". Allowable modes are boolean, byte, character, complex, double, integer, logical, nibble, quad, raw, short, single, ubyte, ushort
+    expect_error(ffTrack(vmode = 'error')) ## Error: Incorrect argument "vmode". Allowable modes are boolean, byte, character, complex, double, integer, logical, nibble, quad, raw, short, single, ubyte, ushort
     expect_error(ffTrack(gr = GRanges()))  ## Error: Trying to create ffTrack with empty GRanges
     ## check 'vmode'
-	## boolean (1 bit logical)
-	## logical (2 bit logical + NA)
-	## quad (2 bit unsigned integer without NA)
-	## nibble (4 bit unsigned integer without NA)
-	## byte (8 bit signed integer with NA)
-	## ubyte (8 bit unsigned integer without NA)
-	## short (16 bit signed integer with NA)
-	## ushort (16 bit unsigned integer without NA)
-	## integer (32 bit signed integer with NA)
-	## single (32 bit float)
-	## double (64 bit float)
-	## raw (8 bit unsigned char)
-	gr = GRanges('1:10000-20000')
+    ## boolean (1 bit logical)
+    ## logical (2 bit logical + NA)
+    ## quad (2 bit unsigned integer without NA)
+    ## nibble (4 bit unsigned integer without NA)
+    ## byte (8 bit signed integer with NA)
+    ## ubyte (8 bit unsigned integer without NA)
+    ## short (16 bit signed integer with NA)
+    ## ushort (16 bit unsigned integer without NA)
+    ## integer (32 bit signed integer with NA)
+    ## single (32 bit float)
+    ## double (64 bit float)
+    ## raw (8 bit unsigned char)
+    gr = GRanges('1:10000-20000')
     expect_error(ffTrack(gr, file.name = 'test.boolean.rds', overwrite = TRUE, vmode = 'boolean'), NA)  ## trick to check code doesn't throw error
     expect_error(ffTrack(gr, file.name = 'test.logical.rds', overwrite = TRUE, vmode = 'logical'), NA)
     expect_error(ffTrack(gr, file.name = 'test.quad.rds', overwrite = TRUE, vmode = 'quad'), NA)
